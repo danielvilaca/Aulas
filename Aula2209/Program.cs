ac037715a34e739890660eb0cf20c22d3f4d91df
Daniel Vilaça <daniel.vilaca98@gmail.com>
2023-09-22 20:47:14 +0100

    Adicionar arquivos de projeto.

---
+++ Aula2209/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Maria" .PadRight(20) + "Barcelos" .PadRight(20));
+Console.WriteLine("Filipe" .PadRight(20) + "Braga" .PadRight(20));
+Console.WriteLine("Manel" .PadRight(20) + "Porto" .PadRight(20));
+
+
+string 
+
+Console.WriteLine("Maria" .PadLeft(20) + "Barcelos" .PadLeft(20));
+Console.WriteLine("Filipe".PadLeft(20) + "Braga" .PadLeft(20));
+Console.WriteLine("Manel".PadLeft(20) + "Porto" .PadLeft(20));
+
