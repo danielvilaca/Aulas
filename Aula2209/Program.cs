2dbd4ce5b73e26d42656a3396f0e6045054c4438
Daniel Vilaça <daniel.vilaca98@gmail.com>
2023-09-22 21:01:54 +0100

    string conc

--- Aula2209/Program.cs
+++ Aula2209/Program.cs
@@ -5,8 +5,15 @@ Console.WriteLine("Maria" .PadRight(20) + "Barcelos" .PadRight(20));
 Console.WriteLine("Filipe" .PadRight(20) + "Braga" .PadRight(20));
 Console.WriteLine("Manel" .PadRight(20) + "Porto" .PadRight(20));
 
+string nome = "Luisa";
+string apelido = "Silva";
 
-string 
+string nomecomp = nome + apelido;
+Console.WriteLine();
+Console.Write(nomecomp .PadLeft(5));
+Console.WriteLine();
+
+
 
 Console.WriteLine("Maria" .PadLeft(20) + "Barcelos" .PadLeft(20));
 Console.WriteLine("Filipe".PadLeft(20) + "Braga" .PadLeft(20));
