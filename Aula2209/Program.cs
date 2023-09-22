3c2e20a9dda52416bf6f2401ad4b4755dc4715d9
Daniel Vilaça <daniel.vilaca98@gmail.com>
2023-09-22 21:48:03 +0100

    Novos dados

--- Aula2209/Program.cs
+++ Aula2209/Program.cs
@@ -17,5 +17,27 @@ Console.WriteLine();
 
 Console.WriteLine("Maria" .PadLeft(20) + "Barcelos" .PadLeft(20));
 Console.WriteLine("Filipe".PadLeft(20) + "Braga" .PadLeft(20));
-Console.WriteLine("Manel".PadLeft(20) + "Porto" .PadLeft(20));
+
+Console.WriteLine();
+
+Console.WriteLine("Ana Ruivo" .PadRight(20) + "Porto" .PadRight(20));
+Console.WriteLine("Joaquina Pinto".PadRight(20) + "Aveiro" .PadRight(20));
+Console.WriteLine("Miguel Costa".PadRight(20) + "Braga" .PadRight(20));
+
+Console.WriteLine();
+
+Console.WriteLine(21.5 + 21.5);
+Console.WriteLine(8.888888888888 / 2);
+Console.WriteLine(1 * 1);
+
+Console.WriteLine();
+
+Console.WriteLine(Math.Pow(2, 2));
+Console.WriteLine(Math.Sqrt(125));
+Console.WriteLine(Math.Exp(2));
+
+Console.WriteLine();
+
+
+
 
