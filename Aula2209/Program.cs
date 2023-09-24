b424049b8c33efdcfdb7cd7580f4c205d1acd72a
Daniel Vilaça <daniel.vilaca98@gmail.com>
2023-09-24 17:33:45 +0100

    Conversão de inteiros / reais, reais / Inteiros

--- Aula2209/Program.cs
+++ Aula2209/Program.cs
@@ -1,42 +1,181 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+//ALINHAMENTO À DIREITA
+Console.WriteLine("ALINHAMENTO À DIREITA");
 Console.WriteLine("Maria" .PadRight(20) + "Barcelos" .PadRight(20));
 Console.WriteLine("Filipe" .PadRight(20) + "Braga" .PadRight(20));
 Console.WriteLine("Manel" .PadRight(20) + "Porto" .PadRight(20));
 
+//CONCATENAÇÃO DE STRINGS
 string nome = "Luisa";
 string apelido = "Silva";
 
 string nomecomp = nome + apelido;
 Console.WriteLine();
-Console.Write(nomecomp .PadLeft(5));
+Console.WriteLine("CONCATENAÇÃO DE STRINGS");
+Console.WriteLine(nomecomp);
 Console.WriteLine();
 
 
-
+//ALINHAMENTO À ESQUERDA
+Console.WriteLine("ALINHAMENTO À ESQUERDA");
 Console.WriteLine("Maria" .PadLeft(20) + "Barcelos" .PadLeft(20));
 Console.WriteLine("Filipe".PadLeft(20) + "Braga" .PadLeft(20));
 
 Console.WriteLine();
 
-Console.WriteLine("Ana Ruivo" .PadRight(20) + "Porto" .PadRight(20));
-Console.WriteLine("Joaquina Pinto".PadRight(20) + "Aveiro" .PadRight(20));
-Console.WriteLine("Miguel Costa".PadRight(20) + "Braga" .PadRight(20));
+//TABULAÇÃO
+Console.WriteLine("TABULAÇÃO");
+Console.WriteLine("Ana Ruivo\t" + "Porto\t");
+Console.WriteLine("Joaquina Pinto\t" + "Aveiro\t");
+Console.WriteLine("Miguel Costa\t" + "Braga\t");
 
 Console.WriteLine();
 
+//OPERAÇÕES ARITMÉTICAS
+Console.WriteLine("OPERAÇÕES ARITMÉTICAS");
 Console.WriteLine(21.5 + 21.5);
 Console.WriteLine(8.888888888888 / 2);
 Console.WriteLine(1 * 1);
 
 Console.WriteLine();
 
+//FUNÇÕES MATEMÁTICAS
+Console.WriteLine("FUNÇÕES MATEMÁTICAS");
 Console.WriteLine(Math.Pow(2, 2));
 Console.WriteLine(Math.Sqrt(125));
 Console.WriteLine(Math.Exp(2));
 
 Console.WriteLine();
+
+//OPERAÇÕES LÓGICAS
+Console.WriteLine("OPERAÇÕES LÓGICAS");
+string teste = "12";
+int num = 12;
+int testeconv;
+
+testeconv = Convert.ToInt32(teste);
+
+bool verifica = testeconv == num;
+Console.WriteLine(verifica);
+
+Console.WriteLine(1 == 2);
+Console.WriteLine(1 != 2);
+Console.WriteLine(3 > 2);
+Console.WriteLine((3 < 4) && (5 > 6));
+Console.WriteLine(3 > 4 || 1 > 2);
+Console.WriteLine(!(4 > 5 || 6 > 7));
+
+Console.WriteLine();
+
+//OPERAÇÕES COM ALFANUMÉRICOS
+Console.WriteLine("OPERAÇÕES COM ALFANUMÉRICOS");
+Console.WriteLine("Lisboa".Length);
+Console.WriteLine("Lisboa é a " + "capital de Portugal");
+Console.WriteLine("Coimbra".ToUpper());
+Console.WriteLine("testeAdelaideteste1teste2".Substring(5, 8));
+Console.WriteLine("      6    ".Trim());
+Console.WriteLine("Lisboa".CompareTo("Lsb"));
+string name = "Braga a melhor cidade do mundo, a segunda é => Porto".Split("=> ")[1];
+Console.WriteLine(name);
+
+
+
+Console.WriteLine();
+
+//NÚMEROS INTEIROS
+Console.WriteLine("NÚMEROS INTEIROS");
+sbyte sb = 34;
+byte b = 250;
+short sh = 32000;
+ushort ush = 60000;
+int i = 100000012;
+uint ui = 3500000000;
+long l = 1532333333334581239;
+ulong ul = 14532333333334581239;
+
+Console.WriteLine(sb + "<" + b + "<" + sh + "<" + ush);
+Console.WriteLine(i + "<" + l + "<" + ui + "<" + ul + "\n");
+
+Console.WriteLine();
+
+//CONVERSÃO DE INTEIROS PARA REAIS E VICEVERSA
+Console.WriteLine("CONVERSÃO DE INTEIROS PARA REAIS E VICEVERSA");
+double d = 99.56;
+int a = (int)d;
+
+int e = 100;
+double f = (double)e;
+
+Console.WriteLine(d + " e " + a);
+Console.WriteLine(f + " e " + e);
+Console.WriteLine();
+
+Console.WriteLine();
+
+//CONVERSÃO DE ALFANUMÉRICOS PARA NUMÉRICOS
+Console.WriteLine("CONVERSÃO DE ALFANUMÉRICOS PARA NUMÉRICOS");
+
+Console.WriteLine();
+
+//CONVERSÃO DE ALFANUMÉRICOS PARA NUMÉRICOS
+Console.WriteLine("CONVERSÃO DE ALFANUMÉRICOS PARA NUMÉRICOS");
+
+Console.WriteLine();
+
+//ARREDONDAMENTOS DE NÚMEROS REAIS
+Console.WriteLine("ARREDONDAMENTOS DE NÚMEROS REAIS");
+
+Console.WriteLine();
+
+//PERCENTAGENS
+Console.WriteLine("PERCENTAGENS");
+
+Console.WriteLine();
+
+//DATAS CRONOLÓGICAS
+Console.WriteLine("DATAS CRONOLÓGICAS");
+
+Console.WriteLine();
+
+//FORMATOS NUMÉRICOS
+Console.WriteLine("FORMATOS NUMÉRICOS");
+
+Console.WriteLine();
+
+//OPERADORES COMBINADOS
+Console.WriteLine("OPERADORES COMBINADOS");
+
+Console.WriteLine();
+
+//LEITURA DE DADOS ALFANUMÉRICOS
+Console.WriteLine("LEITURA DE DADOS ALFANUMÉRICOS");
+
+Console.WriteLine();
+
+//LEITURA DE DADOS NUMÉRICOS
+Console.WriteLine("LEITURA DE DADOS NUMÉRICOS");
+
+Console.WriteLine();
+
+//MAIOR DE DOIS
+Console.WriteLine("MAIOR DE DOIS");
+
+Console.WriteLine();
+
+//MULTIBANCO
+Console.WriteLine("MULTIBANCO");
+
+Console.WriteLine();
+
+//NÚMEROS ÍMPARES
+Console.WriteLine("NÚMEROS ÍMPARES");
+
+Console.WriteLine();
+
+
+
+
 
 
 
